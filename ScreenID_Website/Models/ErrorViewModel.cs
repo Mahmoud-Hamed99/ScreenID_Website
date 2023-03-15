150e9681a76b38a0db5bf43f6a0d16c9b10a0909
MAbdElMoneam <MAbdElMoneam@BlueCloudCorp.Com>
2023-03-15 15:01:44 +0200

    Add project files.

---
+++ ScreenID_Website/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ScreenID_Website.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
